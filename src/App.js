b9eb0983828e10693278cb03db7026ae74530ec2
Luis Damian .M <ldamianmachaca@gmail.com>
2023-03-08 21:07:17 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Hero } from './COMPONENTES/Hero';
+import { Who } from './COMPONENTES/Who';
+import { Works } from './COMPONENTES/Works';
+import { Contact } from './COMPONENTES/Contact';
+import styled from 'styled-components';
+import { Test } from './COMPONENTES/Test';
+
+
+const Container = styled.div`
+  height: 100vh;
+ 
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: white;
+  background: url("./img/bg.jpeg");
+  &::-webkit-scrollbar{
+    display: none;
+  }
+`;
+
+
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Hero/>
+      <Who/>
+      <Works/>
+      <Contact/>
+  
+    </Container>
   );
 }
 
